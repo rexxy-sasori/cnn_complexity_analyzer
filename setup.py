1507e358e339cde52e8d45505b236238be6bd7cc
Rex Geng <hgeng4@illinois.edu>
2021-12-18 13:04:55 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='cnn_complexity_analyzer',  # How you named your package folder (MyLib)
     packages=['cnn_complexity_analyzer'],  # Chose the same as "name"
-    version='0.5',  # Start with a small number and increase it with every change you make
+    version='0.6',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Analyzer computational complexities of CONV layer and FC layers in CNNs',
     # Give a short description about your library
@@ -11,7 +11,7 @@ setup(
     author_email='hgeng4@illinois.edu',  # Type in your E-Mail
     url='https://github.com/rexxy-sasori/cnn_complexity_analyzer',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/rexxy-sasori/cnn_complexity_analyzer/archive/refs/tags/v0.5.tar.gz',  # I explain this later on
+    download_url='https://github.com/rexxy-sasori/cnn_complexity_analyzer/archive/refs/tags/v0.6.tar.gz',  # I explain this later on
     keywords=['Complexity', 'Profiler', 'CNN'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'torch==1.8.1'
